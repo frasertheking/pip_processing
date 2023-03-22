644b740067f510da00a0fe585f5a6370b1d74bdf
Fraser King <frasertheking@gmail.com>
2023-03-22 13:21:30 -0400

    Finished particle table conversion script

--- scripts/conv_pt.py
+++ scripts/conv_pt.py
@@ -11,9 +11,8 @@
 """
 
 ##### Imports
-import os, re, sys
+import os, re
 import pandas as pd
-import xarray as xr
 
 """convert_dist
    This function is used to convert a PIP .dat distribution file to netCDF format
@@ -28,18 +27,12 @@ def convert_particles(filepath, outpath, lat, lon):
 
     f = open(filepath)
     lines = f.readlines()
-    if len(lines) < 14:
+    if len(lines) < 11:
         print("File is empty!")
         return
     
-    year, month, day = re.split(r'\t+', lines[5].rstrip('\t\n'))[0], \
-                        re.split(r'\t+', lines[5].rstrip('\t\n'))[1], \
-                        re.split(r'\t+', lines[5].rstrip('\t\n'))[2]
-    
-    print(year, month, day)
-
     columns = re.split(r'\t+', lines[8].rstrip('\t\n'))[:]
-    units = re.split(r'\t+', lines[9].rstrip('\t\n'))[:]
+    units = ['id', 'nd', 'Site', 'nd', 'Logical', 'nd', 'nd', 'Interval', 'Interval', 'Interval', 'nd', 'mm', 'mm', 'mm^2', 'mm', 'mm', 'deg', 'mm', 'mm', 'mm', 'mm', 'nd', 'nd', 'nd', 'nd', 'mm', 'mm', 'nd', '16_bit', '8_bit']
 
     ##### Parse input
     df = pd.read_csv(filepath, sep='\t', skiprows=range(0, 10), header=0, names=columns)
@@ -56,32 +49,13 @@ def convert_particles(filepath, outpath, lat, lon):
     ds['lat'] = float(lat)
     ds['lon'] = float(lon)
 
-    ##### Compress and save in NETCDF4 format
-    comp = dict(zlib=True, complevel=2)
-    encoding = {var: comp for var in ds.data_vars}
-    print(outpath)
-    ds.to_netcdf(outpath + basename + '.nc', encoding=encoding)
-    print("Conversion complete!")
+    for i, var in enumerate(df.columns):
+        if var == 'time':
+            ds.time.attrs['long_name'] = ['time']
+            continue
 
-    return
-    
-    ##### Define global/variable attributes according to CF-1.10 conventions
-    ds[var].attrs['units'] = units
-    ds[var].attrs['long_name'] = long_name
-    ds[var].attrs['standard_name'] = standard_name
-    ds[var].attrs['missing_value'] = 'NaN'
-    
-    ds.bin_centers.attrs['units'] ='mm'
-    ds.bin_centers.attrs['long_name'] = 'Vertical bin centers'
-    ds.bin_centers.attrs['standard_name'] = 'bin_center'
-    ds.bin_centers.attrs['missing_value'] = 'NaN'
-
-    ds.bin_edges.attrs['units'] ='mm'
-    ds.bin_edges.attrs['long_name'] = 'Vertical bin edges'
-    ds.bin_edges.attrs['standard_name'] = 'bin_edge'
-    ds.bin_edges.attrs['missing_value'] = 'NaN'
-
-    ds.time.attrs['long_name'] = ['time']
+        ds[var].attrs['units'] = units[i]
+        ds[var].attrs['missing_value'] = 'NaN'
 
     ds.attrs['Conventions'] = 'CF-1.10'
     ds.attrs['Contact'] = 'Claire Pettersen (pettersc@umich.edu)'
@@ -91,9 +65,5 @@ def convert_particles(filepath, outpath, lat, lon):
     ##### Compress and save in NETCDF4 format
     comp = dict(zlib=True, complevel=2)
     encoding = {var: comp for var in ds.data_vars}
-    print(outpath)
     ds.to_netcdf(outpath + basename + '.nc', encoding=encoding)
     print("Conversion complete!")
-
-convert_particles('/Users/fraserking/Development/pip_processing/example_data/LakeEffect/PIP/2020_MQT/PIP_3/f_1_2_Particle_Tables_ascii/00620200102/0062020010213200_a_p_60.dat', 
-                  '', 46.53, -87.55)
