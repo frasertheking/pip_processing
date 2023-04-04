501944e38a232d30010b8f493002d1b85fe4b0f9
Fraser King <frasertheking@gmail.com>
2023-04-04 18:36:33 -0400

    Bugfix

--- scripts/mqt_convert.sh
+++ scripts/mqt_convert.sh
@@ -17,6 +17,7 @@ START_YEAR=2014
 END_YEAR=2022
 PIP_PATH="/data/LakeEffect/PIP/"
 TMP_OUT="/data2/fking/s03/converted/"
+CONV_PATH="/data2/fking/s03/temporary/"
 
 declare -a arr=("PIP_3/f_1_4_DSD_Tables_ascii/" "PIP_3/f_2_4_VVD_Tables/" "Study/f_2_6_rho_Plots_D_minute_dat/" "Study/f_3_1_Summary_Tables_P/")
 declare -a wild=("" "_A" "" "") # Need this since VVD has A/S/N filepath pattern
@@ -27,30 +28,50 @@ declare -a units=("m−3 mm−1" "m s-1" "g cm-3" "g cm-3")
 declare -a long=("Drop size distributions" "Vertical velocity distributions" "Effective density distributions" "Effective density")
 declare -a standard=("drop_size_distribution" "velocity_distribution" "effective_density_distribution" "effective_density")
 
+# for y in $(seq $START_YEAR $END_YEAR)
+# do
+#     mkdir -p "${TMP_OUT}${y}_${SHORT}/netCDF/particle_size_distributions/"
+#     mkdir -p "${TMP_OUT}${y}_${SHORT}/netCDF/velocity_distributions/"
+#     mkdir -p "${TMP_OUT}${y}_${SHORT}/netCDF/edensity_distributions/"
+#     mkdir -p "${TMP_OUT}${y}_${SHORT}/netCDF/edensity_lwe_rate/"
+
+#     LOC=0
+#     for i in "${arr[@]}"
+#     do
+#         DATA_PATH="${PIP_PATH}${y}_${SHORT}/"
+#         OUT_PATH="${TMP_OUT}${y}_${SHORT}/netCDF/"
+#         echo "${DATA_PATH}${i}"
+#         for file in "${DATA_PATH}${i}"*"${wild[$LOC]}".dat; do
+#             # echo python dist_wrap.py $file "${OUT_PATH}${vars[$LOC]}/" ${vars[$LOC]} $LAT $LON ${units[$LOC]} ${long[$LOC]} ${standard[$LOC]}
+#             if [[ $LOC -lt 3 ]]; then
+#                 python dist_wrap.py $file "${OUT_PATH}${longnames[$LOC]}/" "${vars[$LOC]}" $LAT $LON "${units[$LOC]}" "${long[$LOC]}" "${standard[$LOC]}" "${SITE}"
+#             else
+#                 python ed_wrap.py $file "${OUT_PATH}${longnames[$LOC]}/" $LAT $LON "${SITE}"
+#             fi
+#             # break
+#         done
+#         (( LOC++ ))
+#     done
+# done
+
+
 for y in $(seq $START_YEAR $END_YEAR)
 do
-    mkdir -p "${TMP_OUT}${y}_${SHORT}/netCDF/particle_size_distributions/"
-    mkdir -p "${TMP_OUT}${y}_${SHORT}/netCDF/velocity_distributions/"
-    mkdir -p "${TMP_OUT}${y}_${SHORT}/netCDF/edensity_distributions/"
-    mkdir -p "${TMP_OUT}${y}_${SHORT}/netCDF/edensity_lwe_rate/"
-
-    LOC=0
-    for i in "${arr[@]}"
-    do
-        DATA_PATH="${PIP_PATH}${y}_${SHORT}/"
-        OUT_PATH="${TMP_OUT}${y}_${SHORT}/netCDF/"
-        echo "${DATA_PATH}${i}"
-        for file in "${DATA_PATH}${i}"*"${wild[$LOC]}".dat; do
-            # echo python dist_wrap.py $file "${OUT_PATH}${vars[$LOC]}/" ${vars[$LOC]} $LAT $LON ${units[$LOC]} ${long[$LOC]} ${standard[$LOC]}
-            if [[ $LOC -lt 3 ]]; then
-                python dist_wrap.py $file "${OUT_PATH}${longnames[$LOC]}/" "${vars[$LOC]}" $LAT $LON "${units[$LOC]}" "${long[$LOC]}" "${standard[$LOC]}" "${SITE}"
-            else
-                python ed_wrap.py $file "${OUT_PATH}${longnames[$LOC]}/" $LAT $LON "${SITE}"
-            fi
-            # break
-        done
-        (( LOC++ ))
+    mkdir -p "${TMP_OUT}${y}_${SHORT}/netCDF/particle_tables/"
+    DATA_PATH="${PIP_PATH}${y}_${SHORT}/"
+    OUT_PATH="${TMP_OUT}${y}_${SHORT}/netCDF/"
+    for dir in "${DATA_PATH}PIP_3/f_1_2_Particle_Tables_ascii/"*/; do
+        if [ -d "$dir" ]; then
+            for filepath in "${dir}"*.zip; do
+                last_dir=$(basename ${dir})
+                mkdir -p "${OUT_PATH}particle_tables/${last_dir}"
+                unzip $filepath -d "${CONV_PATH}${dir}"     # Need to unzip the tables first
+                python pt_wrap.py "${CONV_PATH}${filepath%.zip}" "${OUT_PATH}particle_tables/${last_dir}/" $LAT $LON "${SITE}"
+                rm -r "${CONV_PATH}${filepath%.zip}"    # Delete unzipped file
+            done
+        fi
     done
 done
+
 
 echo "Conversion complete!"
