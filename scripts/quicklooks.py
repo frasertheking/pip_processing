0817b70841457268149410075ba653af3daae31a
Fraser King <frasertheking@gmail.com>
2023-06-14 13:59:23 -0400

    Updates

--- scripts/quicklooks.py
+++ scripts/quicklooks.py
@@ -48,13 +48,20 @@ def create_precip_plots(site):
 #     months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
     for date in matched_dates:
         doy = get_day_of_year(date)
-        ds_lwe = xr.open_dataset(pip_path + '/edensity_lwe_rate/006' + date + '2350_01_P_Minute.nc')    
-        snow = ds_lwe['nrr'].values  
-        rain = ds_lwe['rr'].values
-        ed = ds_lwe['ed'].values
-        avg_snow[doy-1].append(np.nanmean(snow))
-        avg_rain[doy-1].append(np.nanmean(rain))
-        avg_ed[doy-1].append(np.nanmean(ed))
+
+        try:
+            file_pattern = pip_path + str(year) + '_' + site + '/netCDF/edensity_lwe_rate/*' + date + '2350_01_P_Minute.nc'
+            matching_files = glob.glob(file_pattern)
+            ds_lwe = xr.open_dataset(matching_files[0])   
+            snow = ds_lwe['nrr'].values  
+            rain = ds_lwe['rr'].values
+            ed = ds_lwe['ed'].values
+            avg_snow[doy-1].append(np.nanmean(snow))
+            avg_rain[doy-1].append(np.nanmean(rain))
+            avg_ed[doy-1].append(np.nanmean(ed))
+        except FileNotFoundError:
+            print(f"No file found at {pip_path + '/edensity_lwe_rate/006' + date + '2350_01_P_Minute.nc'}")
+
         
     std_snow = [np.nanstd(x) for x in avg_snow]
     std_rain = [np.nanstd(x) for x in avg_rain]
@@ -110,7 +117,9 @@ def create_hists_for_site(site):
         # PIP
 
         try:
-            ds_pip = xr.open_dataset(pip_path + str(year) + '_' + site + '/netCDF/particle_size_distributions/006' + date + '2350_01_dsd.nc')   
+            file_pattern = pip_path + str(year) + '_' + site + '/netCDF/particle_size_distributions/*' + date + '2350_01_dsd.nc'
+            matching_files = glob.glob(file_pattern)
+            ds_pip = xr.open_dataset(matching_files[0])   
             dsd = ds_pip['psd'].values
             dsd_height = np.repeat(np.arange(1, 132), dsd.shape[0])
             dsd_list.append(dsd.T.flatten())
@@ -119,22 +128,26 @@ def create_hists_for_site(site):
             print(f"No file found at {pip_path + str(year) + '_' + site + '/netCDF/particle_size_distributions/006' + date + '2350_01_dsd.nc'}")
 
         try:
-            ds_pip = xr.open_dataset(pip_path + str(year) + '_' + site + '/netCDF/velocity_distributions/006' + date + '2350_01_vvd_A.nc')
+            file_pattern = pip_path + str(year) + '_' + site + '/netCDF/velocity_distributions/*' + date + '2350_01_vvd_A.nc'
+            matching_files = glob.glob(file_pattern)
+            ds_pip = xr.open_dataset(matching_files[0])   
             vvd = ds_pip['vvd'].values
             vvd_height = np.repeat(np.arange(1, 132), vvd.shape[0])
             vvd_list.append(vvd.T.flatten())
             vvd_height_list.append(vvd_height)
         except FileNotFoundError:
-            print(f"No file found at {pip_path + str(year) + '_' + site + '/netCDF/velocity_distributions/006' + date + '2350_01_vvd_A.nc'}")
+            print(f"No file found at {pip_path + str(year) + '_' + site + '/netCDF/velocity_distributions/*' + date + '2350_01_vvd_A.nc'}")
 
         try:
-            ds_pip = xr.open_dataset(pip_path + str(year) + '_' + site + '/netCDF/edensity_distributions/006' + date + '2350_01_rho_Plots_D_minute.nc')   
+            file_pattern =  pip_path + str(year) + '_' + site + '/netCDF/velocity_distributions/*' + date + '2350_01_vvd_A.nc'
+            matching_files = glob.glob(file_pattern)
+            ds_pip = xr.open_dataset(matching_files[0])  
             rho = ds_pip['rho'].values
             rho_height = np.repeat(np.arange(1, 132), rho.shape[0])
             rho_list.append(rho.T.flatten())
             rho_height_list.append(rho_height)
         except FileNotFoundError:
-            print(f"No file found at {pip_path + str(year) + '_' + site + '/netCDF/edensity_distributions/006' + date + '2350_01_rho_Plots_D_minute.nc'}")
+            print(f"No file found at {pip_path + str(year) + '_' + site + '/netCDF/edensity_distributions/*' + date + '2350_01_rho_Plots_D_minute.nc'}")
 
     ze_ds = np.concatenate(ze_list)
     dv_ds = np.concatenate(dv_list)
