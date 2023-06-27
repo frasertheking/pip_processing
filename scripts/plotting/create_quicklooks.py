4305b69a838c50c41db3fbeac94f46e28bc463ef
Fraser King <frasertheking@gmail.com>
2023-06-27 11:55:54 -0400

    Updates

--- scripts/plotting/create_quicklooks.py
+++ scripts/plotting/create_quicklooks.py
@@ -20,6 +20,7 @@ def sanity_check(site, pip_path, mrr_path):
     pip_dates = []
     for year in range(2015, 2023):
         pip_path_temp = os.path.join(pip_path, f"{year}_{site}", "netCDF", "edensity_distributions", "*.nc")
+        print(pip_path_temp)
         for file in glob.glob(pip_path_temp):
             print(file)
             pip_dates.append(file[-37:-29])
