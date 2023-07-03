d8101c0d7c02da259101984b36f26a9ce7b7d668
Fraser King <frasertheking@gmail.com>
2023-07-03 10:42:36 -0400

    Updates

--- scripts/plotting/create_quicklooks.py
+++ scripts/plotting/create_quicklooks.py
@@ -26,6 +26,7 @@ def sanity_check(site, pip_path, mrr_path, match_dates):
 
     matched_dates = []
     files = glob.glob(os.path.join(mrr_path, '*.nc'))
+    print(files)
 
     mrr_dates = []
     for filename in files:
