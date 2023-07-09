caaac4ba42b283641df1f06ff5d3009bbec47e91
Fraser King <frasertheking@gmail.com>
2023-07-09 18:04:08 -0400

    Updates

--- scripts/plotting/site_inter.py
+++ scripts/plotting/site_inter.py
@@ -72,7 +72,7 @@ def plot_n0_lambda():
     axes[1].set_title('$N_{0}$ Distribution')
     axes[1].set_xlim(0, 5)
 
-    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.065), fancybox=True, shadow=True, ncol=7)
+    plt.legend(loc='upper center', bbox_to_anchor=(0, -0.1), fancybox=True, shadow=True, ncol=7)
     plt.tight_layout()
     plt.savefig('../../images/n0_lambda_comparisons.png')
 
