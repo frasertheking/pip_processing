bed712ae629dd006219714691d4e8f6c80ee4f9d
Fraser King <frasertheking@gmail.com>
2023-07-24 18:47:40 -0400

    Updates

---
+++ scripts/conversion/impacts_convert.sh
@@ -0,0 +1,201 @@
+#!/bin/bash
+# file: example_convert.sh
+# author: Fraser King
+# date: March 15, 2023
+
+# Loop through PIP .dat files in a directory and convert them to netCDF
+# using our conv_pip.py utility function. Can convert both distributions
+# and 1D effective density / precipitation rates
+### NOTE: Replace the path and lat/lon/site information for the site you are converting.
+LAT=61.845
+LON=24.287
+SHORT="IMP"
+SITE="Hyytiälä Forestry Field Station"
+START_YEAR=2020
+END_YEAR=2023
+PIP_PATH="/data/Finland/PIP/SN_PIP004/"
+TMP_OUT="/data2/fking/s03/converted/"
+CONV_PATH="/data2/fking/s03/temporary"
+
+
+## MRR2
+            # "westBoundingCoordinate": -73.815,
+            # "eastBoundingCoordinate": -73.814,
+            # "northBoundingCoordinate": 42.681,
+            # "southBoundingCoordinate": 42.68
+
+## MRR PRO
+            # "westBoundingCoordinate": -74.017,
+            # "eastBoundingCoordinate": -73.997,
+            # "northBoundingCoordinate": 40.738,
+            # "southBoundingCoordinate": 40.718
+
+
+declare -a arr=("PIP_3/f_1_4_DSD_Tables_ascii/" "PIP_3/f_2_4_VVD_Tables/" "Study/f_2_6_rho_Plots_D_minute_dat/" "Study/f_3_1_Summary_Tables_P/")
+declare -a wild=("" "_A" "" "") # Need this since VVD has A/S/N filepath pattern
+
+declare -a longnames=("particle_size_distributions" "velocity_distributions" "edensity_distributions" "edensity_lwe_rate")
+declare -a vars=("psd" "vvd" "rho" "ed")
+declare -a units=("m−3 mm−1" "m s-1" "g cm-3" "g cm-3")
+declare -a long=("Drop size distributions" "Vertical velocity distributions" "Effective density distributions" "Effective density")
+declare -a standard=("drop_size_distribution" "velocity_distribution" "effective_density_distribution" "effective_density")
+
+for y in $(seq $START_YEAR $END_YEAR)
+do
+    mkdir -p "${TMP_OUT}${y}_${SHORT}/netCDF/particle_size_distributions/"
+    mkdir -p "${TMP_OUT}${y}_${SHORT}/netCDF/velocity_distributions/"
+    mkdir -p "${TMP_OUT}${y}_${SHORT}/netCDF/edensity_distributions/"
+    mkdir -p "${TMP_OUT}${y}_${SHORT}/netCDF/edensity_lwe_rate/"
+
+    LOC=0
+    for i in "${arr[@]}"
+    do
+        DATA_PATH="${PIP_PATH}${y}_${SHORT}/"
+        OUT_PATH="${TMP_OUT}${y}_${SHORT}/netCDF/"
+        echo "${DATA_PATH}${i}"
+        for file in "${DATA_PATH}${i}"*"${wild[$LOC]}".dat; do
+            # echo python dist_wrap.py $file "${OUT_PATH}${vars[$LOC]}/" ${vars[$LOC]} $LAT $LON ${units[$LOC]} ${long[$LOC]} ${standard[$LOC]}
+            if [[ $LOC -lt 3 ]]; then
+                python dist_wrap.py $file "${OUT_PATH}${longnames[$LOC]}/" "${vars[$LOC]}" $LAT $LON "${units[$LOC]}" "${long[$LOC]}" "${standard[$LOC]}" "${SITE}"
+            else
+                python ed_wrap.py $file "${OUT_PATH}${longnames[$LOC]}/" $LAT $LON "${SITE}"
+            fi
+            # break
+        done
+        (( LOC++ ))
+    done
+done
+
+# for y in $(seq $START_YEAR $END_YEAR)
+# do
+#     mkdir -p "${TMP_OUT}${y}_${SHORT}/netCDF/particle_tables/"
+#     DATA_PATH="${PIP_PATH}${y}_${SHORT}/"
+#     OUT_PATH="${TMP_OUT}${y}_${SHORT}/netCDF/"
+#     for dir in "${DATA_PATH}PIP_3/f_1_2_Particle_Tables_ascii/"*/; do
+#         if [ -d "$dir" ]; then
+#             # handle .zip files
+#             for filepath in "${dir}"*.zip; do
+#                 echo "Found zipfiles"
+#                 echo $filepath
+
+#                 last_dir=$(basename ${dir})
+#                 mkdir -p "${OUT_PATH}particle_tables/${last_dir}"
+#                 mkdir -p "${CONV_PATH}${dir}"
+#                 cp  $filepath -d "${CONV_PATH}${filepath}"
+
+#                 unzip "${CONV_PATH}${filepath}" -d "${CONV_PATH}${filepath%/*}/" # "${CONV_PATH}${filepath%.zip}"   # Need to unzip the tables first
+#                 python pt_wrap.py "${CONV_PATH}${filepath%.zip}" "${OUT_PATH}particle_tables/${last_dir}/" $LAT $LON "${SITE}"
+#                 rm -r "${CONV_PATH}${filepath}"    # Delete unzipped file
+#                 rm -r "${CONV_PATH}${dir}"
+#             done
+
+#             # handle .gz files
+#             for filepath in "${dir}"*.gz; do
+#                 echo "Found gz files"
+#                 last_dir=$(basename ${dir})
+#                 mkdir -p "${OUT_PATH}particle_tables/${last_dir}"
+#                 mkdir -p "${CONV_PATH}${dir}"
+#                 cp  $filepath -d "${CONV_PATH}${filepath}"
+#                 gzip "${CONV_PATH}${filepath}" -d "${CONV_PATH}${filepath%.gz}"   # Need to unzip the tables first
+#                 python pt_wrap.py "${CONV_PATH}${filepath%.gz}" "${OUT_PATH}particle_tables/${last_dir}/" $LAT $LON "${SITE}"
+#                 rm -r "${CONV_PATH}${filepath}"    # Delete unzipped file
+#                 rm -r "${CONV_PATH}${dir}"
+#             done
+
+#             # handle uncompressed files
+#             for filepath in "${dir}"*.dat; do
+#                 echo "Found uncompressed files"
+#                 last_dir=$(basename ${dir})
+#                 mkdir -p "${OUT_PATH}particle_tables/${last_dir}"
+#                 python pt_wrap.py "${filepath}" "${OUT_PATH}particle_tables/${last_dir}/" $LAT $LON "${SITE}"
+#             done
+
+#         fi
+#     done
+# done
+
+# PIP_2
+# for y in $(seq $START_YEAR $END_YEAR)
+# do
+#     mkdir -p "${TMP_OUT}${y}_${SHORT}/netCDF/a_particle_tables/"
+#     DATA_PATH="${PIP_PATH}${y}_${SHORT}/"
+#     OUT_PATH="${TMP_OUT}${y}_${SHORT}/netCDF/"
+#     for dir in "${DATA_PATH}PIP_2/a_Particle_Tables/"*/; do
+#         if [ -d "$dir" ]; then
+#             # handle .zip files
+#             for filepath in "${dir}"*.zip; do
+#                 echo "Found zipfiles"
+#                 echo $filepath
+#                 last_dir=$(basename ${dir})
+
+#                 # define output file name based on the input file (assuming output extension is .nc)
+#                 outfile="${OUT_PATH}a_particle_tables/${last_dir}/$(basename "${filepath%.*.*}").nc"
+
+#                 # if output file already exists, skip to the next iteration
+#                 if [ -f "$outfile" ]; then
+#                     echo "Skipping already processed file: $filepath"
+#                     continue
+#                 fi
+
+#                 mkdir -p "${OUT_PATH}a_particle_tables/${last_dir}"
+#                 mkdir -p "${CONV_PATH}${dir}"
+#                 cp  $filepath -d "${CONV_PATH}${filepath}"
+
+#                 unzip "${CONV_PATH}${filepath}" -d "${CONV_PATH}${filepath%/*}/" # "${CONV_PATH}${filepath%.zip}"   # Need to unzip the tables first
+#                 python pt_wrap.py "${CONV_PATH}${filepath%.zip}" "${OUT_PATH}a_particle_tables/${last_dir}/" $LAT $LON "${SITE}"
+#                 rm -r "${CONV_PATH}${filepath}"    # Delete unzipped file
+#                 rm -r "${CONV_PATH}${dir}"
+#             done
+
+#             # handle .gz files
+#             for filepath in "${dir}"*.gz; do
+#                 echo "Found gz files"
+#                 echo $filepath
+
+#                 last_dir=$(basename ${dir})
+
+#                 # define output file name based on the input file (assuming output extension is .nc)
+#                 outfile="${OUT_PATH}a_particle_tables/${last_dir}/$(basename "${filepath%.*.*}").nc"
+
+#                 # if output file already exists, skip to the next iteration
+#                 if [ -f "$outfile" ]; then
+#                     echo "Skipping already processed file: $filepath"
+#                     continue
+#                 fi
+
+#                 mkdir -p "${OUT_PATH}a_particle_tables/${last_dir}"
+#                 mkdir -p "${CONV_PATH}${dir}"
+#                 cp  $filepath -d "${CONV_PATH}${filepath}"
+#                 gzip "${CONV_PATH}${filepath}" -d "${CONV_PATH}${filepath%.gz}"   # Need to unzip the tables first
+#                 python pt_wrap.py "${CONV_PATH}${filepath%.gz}" "${OUT_PATH}a_particle_tables/${last_dir}/" $LAT $LON "${SITE}"
+#                 rm -r "${CONV_PATH}${filepath}"    # Delete unzipped file
+#                 rm -r "${CONV_PATH}${dir}"
+#             done
+
+#             # handle uncompressed files
+#             for filepath in "${dir}"*.dat; do
+#                 echo "Found uncompressed files"
+#                 echo $filepath
+#                 last_dir=$(basename ${dir})
+
+#                 # define output file name based on the input file (assuming output extension is .nc)
+#                 outfile="${OUT_PATH}a_particle_tables/${last_dir}/$(basename "${filepath%.*}").nc"
+#                 echo $outfile
+
+#                 # if output file already exists, skip to the next iteration
+#                 if [ -f "$outfile" ]; then
+#                     echo "Skipping already processed file: $filepath"
+#                     continue
+#                 fi
+
+#                 mkdir -p "${OUT_PATH}a_particle_tables/${last_dir}"
+#                 python pt_wrap.py "${filepath}" "${OUT_PATH}a_particle_tables/${last_dir}/" $LAT $LON "${SITE}"
+#             done
+
+#         fi
+#     done
+# done
+
+
+
+echo "Conversion complete!"
---
+++ scripts/pca_preprocess.py
@@ -0,0 +1,280 @@
+import glob, os
+import xarray as xr
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import warnings
+
+from datetime import datetime, timedelta
+from scipy.optimize import curve_fit
+warnings.filterwarnings("ignore")
+warnings.simplefilter(action='ignore', category=FutureWarning)
+plt.rcParams.update({'font.size': 15})
+
+def get_cloud_positions(data):
+    cloud_bases = []
+    cloud_tops = []
+
+    for col in data.T:  # Transpose to loop over columns
+        cloud_base = np.nan
+        cloud_top = np.nan
+        count = 0
+        
+        for i in range(len(col)):
+            if not np.isnan(col[i]):
+                if np.isnan(cloud_base):  # Start of the cloud
+                    cloud_base = i
+                count += 1
+            else:
+                if not np.isnan(cloud_base) and count >= 4:  # End of the cloud
+                    cloud_top = i - 1  # The last non-NaN value
+                    break
+                else:  # Reset
+                    cloud_base = np.nan
+                    count = 0
+
+        if not np.isnan(cloud_base) and (cloud_top is None or np.isnan(cloud_top)):
+            # In case the cloud goes up to the last bin in the column
+            if count >= 4:
+                cloud_top = len(col) - 1
+            else:
+                # Cloud size is less than 4, reset
+                cloud_base = np.nan
+                
+        cloud_bases.append(cloud_base)
+        cloud_tops.append(cloud_top)
+
+    return np.array(cloud_bases)[:283], np.array(cloud_tops)[:283], (np.array(cloud_tops) - np.array(cloud_bases))[:283]
+
+
+def calc_various_pca_inputs(site):
+    print("Working on site " + site)
+
+    ### Globals
+    pip_path = '/data/LakeEffect/PIP/Netcdf_Converted/'
+    mrr_path = '/data/LakeEffect/MRR/NetCDF_DN/'
+    met_path = '/data/LakeEffect/MetData/'
+    NANO_FACTOR = 1000000000
+    MIN_INTERVALS_5 = 283
+
+    ### Get matched dates
+    ds_met = xr.open_dataset(met_path + '2013-2020_' + site + '.nc')
+    times = np.asarray((ds_met['UTC Time'].values / NANO_FACTOR), dtype=int)
+    utc_time = pd.to_datetime(times, unit='s', origin='unix')
+    ds_met = ds_met.assign_coords(time = utc_time)
+
+    mrr_dates = []
+    for file in list(glob.glob(mrr_path + '*.nc')):                                       
+        mrr_dates.append(file[-16:-8])
+        
+    pip_dates = []
+    for file in glob.glob(os.path.join(pip_path, '**', 'edensity_distributions', '*.nc'), recursive=True):
+        pip_dates.append(file[-37:-29])
+
+    mrr_ds_dates = []
+    for date in mrr_dates:
+        year = int(date[:4])
+        month = int(date[4:6])
+        day = int(date[-2:])
+        ds = ds_met.sel(time=(ds_met['time.year']==year) & (ds_met['time.month']==month) & (ds_met['time.day']==day))
+        
+        if len(ds.time.values) > 0:
+            mrr_ds_dates.append(date)
+            
+    met_dates = []
+    base_date = datetime.strptime('20190101', '%Y%m%d')
+    for i in range(365):
+        new_date = base_date + timedelta(days=i)
+        year = new_date.year
+        month = new_date.month
+        day = new_date.day
+        ds = ds_met.sel(time=(ds_met['time.year']==year) & (ds_met['time.month']==month) & (ds_met['time.day']==day))
+        
+        if len(ds.time.values) > 0:
+            met_dates.append(str(year) + str(month) + str(day))
+
+    matched_dates = []
+    for date in mrr_ds_dates:
+        files = glob.glob(pip_path + '/*/*/*' + date + '*.nc')
+        if len(files) == 4:
+            matched_dates.append(date)
+            
+    print("MRR:", len(mrr_dates), "MET:", len(met_dates), "PIP:", len(pip_dates))
+    print("Matched:", len(matched_dates))
+    print(matched_dates)
+
+    N_0_array = []
+    lambda_array = []
+    total_particle_array = []
+    avg_ed_array = []
+    avg_sr_array = []
+    avg_vvd_array = []
+    mwd_array = []
+    temp_array = []
+    rh_array = []
+    ws_array = []
+    press_array = []
+    wd_array = []
+    base_array = []
+    top_array = []
+    depth_array = []
+
+    number_of_files = 0
+    for matched_date in matched_dates:
+        print("Working on day", matched_date)
+        year = int(matched_date[:4])
+        month = int(matched_date[4:6])
+        day = int(matched_date[-2:])
+
+        # Load MET data
+        ds_met_day = ds_met.sel(time=(ds_met['time.year']==year) & (ds_met['time.month']==month) & (ds_met['time.day']==day))
+
+        # Load MRR data
+        ds_mrr_day = xr.open_dataset(mrr_path + 'MRR_NWS_MQT_' + matched_date + '_snow.nc')
+
+        # Load PIP data
+        ds_edensity_lwe_rate = xr.open_dataset(pip_path + str(year) + '_' + site + '/edensity_lwe_rate/006' + matched_date + '2350_01_P_Minute.nc')
+        ds_edensity_distributions = xr.open_dataset(pip_path + str(year) + '_' + site + '/edensity_distributions/006' + matched_date + '2350_01_rho_Plots_D_minute.nc')
+        ds_velocity_distributions = xr.open_dataset(pip_path + str(year) + '_' + site + '/velocity_distributions/006' + matched_date + '2350_01_vvd_A.nc')
+        ds_particle_size_distributions = xr.open_dataset(pip_path + str(year) + '_' + site + '/particle_size_distributions/006' + matched_date + '2350_01_dsd.nc')
+
+        dsd_values = ds_particle_size_distributions['psd'].values
+        edd_values = ds_edensity_distributions['rho'].values
+        vvd_values = ds_velocity_distributions['vvd'].values
+        sr_values = ds_edensity_lwe_rate['nrr'].values
+        ed_values = ds_edensity_lwe_rate['ed'].values
+        bin_centers = ds_particle_size_distributions['bin_centers'].values
+
+        if len(ds_particle_size_distributions.time) != 1440:
+            print("PIP data record too short for day, skipping!")
+            continue
+
+        # Print results
+        # print("\nMET")
+        # print(ds_met_day)
+
+        # print("\nMRR")
+        # print(ds_mrr_day)
+
+        # print("\nPIP")
+        # print(ds_edensity_lwe_rate)
+        # print(ds_edensity_distributions)
+        # print(ds_velocity_distributions)
+        # print(ds_particle_size_distributions)
+
+        if len(ds_met_day['Temp Out'].values[:MIN_INTERVALS_5]) != 283:
+            print("MET data record too short for day, skipping!")
+            continue
+
+        ########## MRR CALCULATIONS
+        data = ds_mrr_day['Ze'].values.T[:,:1440]
+        if data.shape[1] < 1440:
+            print("MRR data record too short for day, skipping!")
+            continue
+
+        data_reshaped = data.reshape(31, 1440 // 5, 5)
+        data = np.nanmean(data_reshaped, axis=-1)
+        mask = data < -5
+        data[mask] = np.nan
+        cloud_bases, cloud_tops, cloud_depths = get_cloud_positions(data)
+
+        # Plot MRR data
+        # fig, ax = plt.subplots(figsize=(12,3))
+        # plt.imshow(data, cmap = cm.gist_ncar, vmin = -10, vmax = 30, aspect='auto', interpolation='bilinear')
+        # plt.axhline(y=2, color='gray', linestyle='--')
+        # plt.scatter(np.arange(len(cloud_bases)), cloud_bases, color='blue', marker='x')
+        # plt.scatter(np.arange(len(cloud_tops)), cloud_tops, color='red', marker='x')
+        # plt.plot(np.arange(len(cloud_depths)), cloud_depths, color='black', linestyle='--')
+        # plt.gca().invert_yaxis()
+        # plt.show()
+
+        base_array.append(cloud_bases)
+        top_array.append(cloud_tops)
+        depth_array.append(cloud_depths)
+
+        ########## PIP CALCULATIONS
+        func = lambda t, a, b: a * np.exp(-b*t)
+
+        # Loop over each 5-minute block
+        count = 0
+        for i in range(0, dsd_values.shape[0], 5):
+            if i >= 1415:
+                continue
+
+            count += 1
+            block_avg = np.mean(dsd_values[i:i+5, :], axis=0)
+            valid_indices = ~np.isnan(block_avg)
+            block_avg = block_avg[valid_indices]
+            valid_bin_centers = bin_centers[valid_indices]
+
+            if block_avg.size == 0:
+                N_0_array.append(np.nan)
+                lambda_array.append(np.nan)
+                avg_vvd_array.append(np.nan)
+                avg_ed_array.append(np.nan)
+                avg_sr_array.append(np.nan)
+                total_particle_array.append(0)
+                mwd_array.append(np.nan)
+                continue
+
+            # Calculate average fallspeed over the 5-minute interval
+            avg_vvd_array.append(np.nanmean(vvd_values[i:i+5, :], axis=(0, 1)))
+
+            # Calculate the average eDensity of the 5-minute interval
+            avg_ed_array.append(np.nanmean(ed_values[i:i+5]))
+
+            # Calculate the average snowfall rate over the 5-minute interval
+            avg_sr_array.append(np.nanmean(sr_values[i:i+5]))
+
+            # Calculate total number of particles over the 5-minute interval
+            total_particle_array.append(np.nansum(dsd_values[i:i+5, :], axis=(0, 1)))
+
+            # Calculate mean mass diameter over the 5-minute interval
+            mass_dist = edd_values[i:i+5, valid_indices] * dsd_values[i:i+5, valid_indices] * (4/3) * np.pi * (valid_bin_centers/2)**3
+            mass_weighted_diameter = np.sum(mass_dist * valid_bin_centers) / np.sum(mass_dist)
+            mwd_array.append(mass_weighted_diameter)
+
+            # Calculate N0 and Lambda
+            try:
+                popt, pcov = curve_fit(func, valid_bin_centers, block_avg, p0 = [1e4, 2], maxfev=600)
+                if popt[0] > 0 and popt[0] < 10**7 and popt[1] > 0 and popt[1] < 10:
+                    N_0_array.append(popt[0])
+                    lambda_array.append(popt[1])
+                else:
+                    N_0_array.append(np.nan)
+                    lambda_array.append(np.nan)
+
+            except RuntimeError:
+                N_0_array.append(np.nan)
+                lambda_array.append(np.nan)
+
+
+        ########## MET CALCULATIONS
+        temp_array.append(ds_met_day['Temp Out'].values[:MIN_INTERVALS_5])
+        rh_array.append(ds_met_day['RH Out'].values[:MIN_INTERVALS_5])
+        ws_array.append(ds_met_day['Wind Speed'].values[:MIN_INTERVALS_5])
+        press_array.append(ds_met_day['Pressure'].values[:MIN_INTERVALS_5])
+        wd_array.append(ds_met_day['Wind Dir'].values[:MIN_INTERVALS_5])
+        number_of_files += 1
+
+    top_array = [item for sublist in top_array for item in sublist]
+    base_array = [item for sublist in base_array for item in sublist]
+    depth_array = [item for sublist in depth_array for item in sublist]
+    temp_array = [item for sublist in temp_array for item in sublist]
+    rh_array = [item for sublist in rh_array for item in sublist]
+    ws_array = [item for sublist in ws_array for item in sublist]
+    press_array = [item for sublist in press_array for item in sublist]
+    wd_array = [item for sublist in wd_array for item in sublist]
+
+
+    df = pd.DataFrame(data={'n0': N_0_array,  'D0': mwd_array, 'Nt': total_particle_array, \
+                            'Fs': avg_vvd_array, 'Sr': avg_sr_array,  'Ed': avg_ed_array, \
+                            'lambda': lambda_array, 'Ct': top_array, 'Cb': base_array, \
+                            'Cd': depth_array, 't': temp_array, 'q': rh_array, 'Ws': ws_array, \
+                            'u': press_array, 'Wd': wd_array})
+    df.to_csv('/data2/fking/s03/data/processed/pca_inputs/' + site + '.csv')
+
+calc_various_pca_inputs('MQT')
+
