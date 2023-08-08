ffa65221721119e2e61c46fa328742deee6e8675
Fraser King <frasertheking@gmail.com>
2023-08-08 14:54:27 -0400

    updates

--- scripts/pca_pip_only_preprocess.py
+++ scripts/pca_pip_only_preprocess.py
@@ -139,7 +139,14 @@ def calc_various_pca_inputs(site):
 
             fig, ax = plt.subplots(figsize=(12,12))
             plt.plot(np.arange(len(ed_values[i:i+5])), ed_values[i:i+5])
+            print(ed_values[i:i+5])
             plt.savefig('test.png')
+
+            fig, ax = plt.subplots(figsize=(12,12))
+            plt.plot(np.arange(len(edd_values[i:i+5])), edd_values[i:i+5])
+            print(edd_values[i:i+5])
+            plt.savefig('test_edd.png')
+
             sys.exit()
 
             # Calculate the average eDensity of the 5-minute interval
