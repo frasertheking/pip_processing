e2ed4ebcaa4cb292ce2fae8cb973cdf0143a00f4
Fraser King <frasertheking@gmail.com>
2023-08-21 16:05:46 -0400

    Updates

--- ed_timing/compare_data.py
+++ ed_timing/compare_data.py
@@ -2,8 +2,11 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import datashader as ds
-from datashader.mpl_ext import dsshow
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.colors import Normalize 
+from scipy.interpolate import interpn
 
 MAIN_PATH = '/data2/fking/s03/converted/'
 LAST_PATH = '/netCDF/adjusted_edensity_lwe_rate/'
@@ -38,35 +41,32 @@ valid_ed_fixed = merged_data['adj_ed']
 correlation_ed = merged_data['rho'].corr(merged_data['ed'])
 correlation_adj_ed = merged_data['rho'].corr(merged_data['adj_ed'])
 
+def density_scatter( x , y, ax = None, sort = True, bins = 20, **kwargs )   :
+    """
+    Scatter plot colored by 2d histogram
+    """
+    if ax is None :
+        fig , ax = plt.subplots()
+    data , x_e, y_e = np.histogram2d( x, y, bins = bins, density = True )
+    z = interpn( ( 0.5*(x_e[1:] + x_e[:-1]) , 0.5*(y_e[1:]+y_e[:-1]) ) , data , np.vstack([x,y]).T , method = "splinef2d", bounds_error = False)
 
-def using_datashader(ax, x, y):
+    #To be sure to plot all data
+    z[np.where(np.isnan(z))] = 0.0
 
-    df = pd.DataFrame(dict(x=x, y=y))
-    dsartist = dsshow(
-        df,
-        ds.Point("x", "y"),
-        ds.count(),
-        cmap='inferno',
-        norm="log",
-        vmin=1,
-        vmax=250,
-        aspect="auto",
-        ax=ax,
-    )
+    # Sort the points by density, so that the densest points are plotted last
+    if sort :
+        idx = z.argsort()
+        x, y, z = x[idx], y[idx], z[idx]
 
-    plt.colorbar(dsartist)
+    ax.scatter( x, y, c=z, **kwargs )
 
+    norm = Normalize(vmin = np.min(z), vmax = np.max(z))
+    cbar = fig.colorbar(cm.ScalarMappable(norm = norm), ax=ax)
+    cbar.ax.set_ylabel('Density')
 
-fig, ax = plt.subplots(figsize=(10,10))
-using_datashader(ax, valid_rho, valid_ed)
-ax.set_facecolor('black')
-plt.savefig('datashader1.png')
+    return ax
 
-fig, ax = plt.subplots(figsize=(10,10))
-using_datashader(ax, valid_rho, valid_ed_fixed)
-ax.set_facecolor('black')
-plt.savefig('datashader2.png')
-
+density_scatter(valid_ed, valid_rho, bins = [200,200])
 
 # # Create a 1x2 subplot layout
 # fig, axarr = plt.subplots(1, 2, figsize=(16,8))
