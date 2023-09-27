9d1946f0f63d9a408b8f92850a6fc539d07786b0
Fraser King <frasertheking@gmail.com>
2023-09-27 12:46:12 -0400

    Updates

--- scripts/rename_files_final.sh
+++ scripts/rename_files_final.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the root path
-ROOT_PATH="/data2/fking/s03/converted/2018_NSA"
+ROOT_PATH="/data2/fking/s03/converted"
 
 # Define subdirectories and suffixes
 SUBDIRS=("adjusted_edensity_lwe_rate" "particle_size_distributions" "edensity_distributions" "velocity_distributions")
@@ -11,6 +11,14 @@ SUFFIXES=("_min" "_dsd" "_rho" "_vvd")
 for SUBFOLDER in "$ROOT_PATH"/*/; do
     # Check if it's a directory before proceeding
     if [ -d "$SUBFOLDER" ]; then
+
+        # Check if 'NSA' is in the $SUBFOLDER name
+        if [[ "$SUBFOLDER" == *NSA* ]]; then
+            # If NSA is found, continue to the next iteration of the loop
+            echo "Skipping $SUBFOLDER due to NSA in name"
+            continue
+        fi
+
         # Loop through each subdirectory type
         for INDEX in "${!SUBDIRS[@]}"; do
             SUBDIR="${SUBDIRS[$INDEX]}"
